5cb3c79d3e64e8f6da23ee909922ca6723e24d29
Sathisha NM <sathisha.nm@gmail.com>
2021-08-08 22:02:03 +0530

    Improvise Cart assign Part

--- DiscountEngine.UnitTets/CartTest.cs
+++ DiscountEngine.UnitTets/CartTest.cs
@@ -1,18 +1,29 @@
+using DiscountEngine.Models;
+using DiscountEngine.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace DiscountEngine.UnitTets
 {
     [TestClass]
     public class CartTest
     {
+        private List<Promotion> _promotions;
+
         [TestInitialize]
         public void Initialize()
         {
+            // Need to be configurable on real scenario -DB,through UI,XML/json file, etc 
+            ProductPriceServices.ProductPricesInfo = PromoEngineTestHelper.ProductPricesInfo;
+            _promotions = PromoEngineTestHelper.Promotions;
+
         }
 
         [TestCleanup]
         public void TestClean()
         {
+            ProductPriceServices.ProductPricesInfo = null;
+            _promotions = null;
 
         }
 
@@ -20,14 +31,19 @@ namespace DiscountEngine.UnitTets
         public void GetOrderBillDetailsFinalPriceSecenario1Test()
         {
             // Assign
+            List<CartOrder> orders = new List<CartOrder>();
+            CartOrder order = new CartOrder(1, new List<Product>() {
+                new Product('A'), new Product('B'), new Product('C')
+            });
+
             Cart cart = new Cart();
             cart.AddOrders();
 
             // Act
-            var expectedFinalPrice = cart.GetOrderBillDetails(); ;
+            var orderBillDeatails = cart.GetOrderBillDetails();
 
             //Assert
-            Assert.AreEqual(100, expectedFinalPrice);
+            Assert.AreEqual(100, orderBillDeatails);
         }
     }
 }
--- DiscountEngine/Models/CartOrder.cs
+++ DiscountEngine/Models/CartOrder.cs
@@ -1,19 +1,18 @@
-﻿using System;
+﻿using DiscountEngine.Models;
+using System;
 using System.Collections.Generic;
-using System.Text;
-
 namespace DiscountEngine
 {
     public class CartOrder
     {
         public int OrderId { get; set; }
-        public List<string> Products { get; set; }
+        public List<Product> Products { get; set; }
 
-        public CartOrder(int orderId, List<string> products)
+        public CartOrder(int orderId, List<Product> products)
         {
             OrderId = orderId;
             Products = products;
         }
-       
+
     }
 }
