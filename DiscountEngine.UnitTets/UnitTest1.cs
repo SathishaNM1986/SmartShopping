c3011a78885c825e6b4563d88b1ccdd3ec6d3507
Sathisha NM <sathisha.nm@gmail.com>
2021-08-08 20:48:24 +0530

    Framework Checkin-TestProjects

---
+++ DiscountEngine.UnitTets/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DiscountEngine.UnitTets
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
