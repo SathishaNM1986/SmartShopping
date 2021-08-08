ba6cd100fedfc33e3cad07ac05f56510538da856
Sathisha NM <sathisha.nm@gmail.com>
2021-08-08 22:36:19 +0530

    GroupingPromotion  test cases  and core logic implementation

--- DiscountEngine.UnitTets/Services/GroupingPromotionTest.cs
+++ DiscountEngine.UnitTets/Services/GroupingPromotionTest.cs
@@ -1,10 +1,54 @@
-﻿using System;
+﻿using DiscountEngine.Models;
+using DiscountEngine.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace DiscountEngine.UnitTets.Services
 {
-    class GroupingPromotionTest
+    [TestClass]
+    public class GroupingPromotionTest
     {
+        private List<Promotion> _promotions;
+        private GroupingPromotion _groupingPromotion;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Need to be configurable on real scenario -DB,through UI,XML/json file, etc 
+            ProductPriceServices.ProductPricesInfo = PromoEngineTestHelper.ProductPricesInfo;
+            _promotions = PromoEngineTestHelper.Promotions;
+        }
+
+        [TestCleanup]
+        public void TestClean()
+        {
+            ProductPriceServices.ProductPricesInfo = null;
+            _promotions = null;
+            _groupingPromotion = null;
+
+        }
+
+        [TestMethod]
+        [DataRow(0, 20)]
+        [DataRow(1, 30)]
+        [DataRow(2, 5)]
+        public void GetDiscountPriceTest(int promotionNumber, double expectedDiscountPrice)
+        {
+            // Assign
+            _groupingPromotion = new GroupingPromotion();
+            CartOrder order = new CartOrder(1, new List<Product>() {
+                new Product('A'),new Product('A'),new Product('A'),
+                new Product('B'),new Product('B'),new Product('B'),new Product('B'),new Product('B'),
+                new Product('C'),
+                new Product('D')
+            });
+
+            // Act
+            var acutalDiscountPrice = _groupingPromotion.GetDiscountPrice(order, _promotions[promotionNumber]);
+
+            //Assert
+            Assert.AreEqual(Convert.ToDecimal(expectedDiscountPrice), acutalDiscountPrice);
+        }
     }
 }
--- DiscountEngine/Services/GroupingPromotion.cs
+++ DiscountEngine/Services/GroupingPromotion.cs
@@ -1,8 +1,7 @@
 ﻿using DiscountEngine.Interfaces;
 using DiscountEngine.Models;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace DiscountEngine.Services
 {
@@ -10,7 +9,44 @@ namespace DiscountEngine.Services
     {
         public decimal GetDiscountPrice(CartOrder order, Promotion promotion)
         {
-            return 100;
+            decimal discountAmount = 0;
+            try
+            {
+                // Group by product/skuid 
+                var grpProducts = order.Products.GroupBy(x => x.SkuId);
+                // promo eligible
+                var promoeligibleItems = grpProducts.Where(grp => promotion.ProductInfo
+                    .Any(promoProduct => grp.Key == promoProduct.Key && grp.Count() >= promoProduct.Value));
+                if (promoeligibleItems.Any())
+                {
+                    var promoEligibleItemsCount = promoeligibleItems.Select(grp => grp.Count()).Sum();
+
+                    // read the total count of promoted products from promotion
+                    int promoPreDefinedQuantitySum = promotion.ProductInfo.Sum(prodinfo => prodinfo.Value);
+
+                    var result = promoeligibleItems.ToDictionary(group => group.Key, group => group.ToList()).Values;
+                    decimal totalUnitPrice = 0;
+                    foreach (var item in result)
+                    {
+                        foreach (var product in item.Take(promoPreDefinedQuantitySum))
+                        {
+                            totalUnitPrice += product.Price;
+                        }
+                    }
+                    while (promoPreDefinedQuantitySum > 0 && promoEligibleItemsCount >= promoPreDefinedQuantitySum)
+                    {
+                        discountAmount += (totalUnitPrice - promotion.PromoPrice);
+                        promoEligibleItemsCount -= promoPreDefinedQuantitySum;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // Error handling and logging
+                // use some logging mechanism like log4 net ...etc 
+            }
+            return discountAmount;
         }
     }
 }
+
