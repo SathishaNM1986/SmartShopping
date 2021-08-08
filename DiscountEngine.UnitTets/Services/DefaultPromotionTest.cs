bfa358c949697de22f14d3c3c74677602cc1b40a
Sathisha NM <sathisha.nm@gmail.com>
2021-08-08 22:24:26 +0530

    Null design pattren

---
+++ DiscountEngine.UnitTets/Services/DefaultPromotionTest.cs
@@ -0,0 +1,27 @@
+﻿using DiscountEngine.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscountEngine.UnitTets.Services
+{
+    [TestClass]
+    public class DefaultPromotionTest
+    {
+        private DefaultPromotion _defaultPromotion;
+
+        [TestMethod]
+        public void GetDiscountPriceTest()
+        {
+            // Assign
+            _defaultPromotion = new DefaultPromotion();
+
+            // Act
+            var acutalDiscountPrice = _defaultPromotion.GetDiscountPrice(null, null);
+
+            //Assert
+            Assert.AreEqual(0, acutalDiscountPrice);
+        }
+    }
+}
