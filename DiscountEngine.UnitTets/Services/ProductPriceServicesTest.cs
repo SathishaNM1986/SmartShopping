51db4ead95e3cb3b8aa80cd006846e65eea43c31
Sathisha NM <sathisha.nm@gmail.com>
2021-08-08 21:56:29 +0530

    ProductSer Tests & implemen

---
+++ DiscountEngine.UnitTets/Services/ProductPriceServicesTest.cs
@@ -0,0 +1,20 @@
+﻿using DiscountEngine.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscountEngine.UnitTets.Services
+{
+    [TestClass]
+    public class ProductPriceServicesTest
+    {
+        [TestMethod]
+        public void ProductPricesInfoSetAndGetTest()
+        {
+            ProductPriceServices.ProductPricesInfo = PromoEngineTestHelper.ProductPricesInfo;
+            Assert.IsNotNull(ProductPriceServices.ProductPricesInfo);
+
+        }
+    }
+}
---
+++ DiscountEngine/Services/ProductPriceServices.cs
@@ -0,0 +1,10 @@
+﻿using DiscountEngine.Models;
+using System.Collections.Generic;
+
+namespace DiscountEngine.Services
+{
+    public static class ProductPriceServices
+    {
+        public static List<Product> ProductPricesInfo { get; set; }
+    }
+}
